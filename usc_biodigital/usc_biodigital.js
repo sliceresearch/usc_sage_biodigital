5be15bda56a825db23a189a26e7bd3c430d5ecbd
Levi Kotzur <lkotzur@usc.edu.au>
2017-06-07 14:32:51 +1000

    Added radio widget to change biodigital model, also cleaned code of old comments

--- usc_biodigital/usc_biodigital.js
+++ usc_biodigital/usc_biodigital.js
@@ -30,27 +30,17 @@ var usc_biodigital = SAGE2_App.extend({
 		console.log('usc_biodigital> id=', this.id, 'init element=', this.element,
 		    'w=', this.element.clientWidth, 'h=', this.element.clientHeight);
 		
-		/*
-		// adding an svg to the element
-		this.container = d3.select(this.element)
-			.append("svg")
-			.attr("id", "biodigitalSVG")
-			.attr("width", this.element.clientWidth)
-			.attr("height", 0.1 * this.element.clientHeight);
-		*/
-		
-		// generate the interface of the usc_biodigital
-		//this.createBioInterface();
+		// generate the widget interface of the usc_biodigital
 		this.addWidgetButtons();
 
 		// Set the background to black
-		this.element.style.backgroundColor = '#ADD8E6';			
+		this.element.style.backgroundColor = '#000000';			
 		
 		var iframe = document.createElement('iframe');
 		iframe.src = this.state.value;
 		iframe.id = IFRAME_ID + this.id;
 		iframe.width =  '100%';
-		iframe.height =  '100%';//data.height - 0.1 * this.element.clientHeight - 6;
+		iframe.height =  '100%';
 		iframe.setAttribute("style", "float:left");
 
 		this.element.appendChild(iframe);
@@ -83,112 +73,7 @@ var usc_biodigital = SAGE2_App.extend({
 		// Control the frame rate for an animation application
 		this.maxFPS = 2.0;
 	},
-
-	// functionused to create the interface of the timer
-	createBioInterface: function() {
-		// clear the current svg
-		this.container.selectAll("*").remove();
-
-		// setting the number of rows, cols and the padding size of the table
-		var nRows   = 4;
-		var nCols   = 4;
-		var padding = 3;
-
-		// setting the image path
-		var path = this.resrcPath + "/img/";
-
-		// creating the model of the interface that will be given and interpreted by d3 to create the interface
-		this.modelInterface = [
-			{ name: "Quiz" + this.id, command: "true", group: 0, parent: this, action: this.btnQuizClick, text: "Quiz", r: 0, c: 3, cSpan: 1, rSpan: 1 },
-			
-			{ name: "Normal" + this.id, command: "true", group: 1, parent: this, action: this.btnNormalClick, text: "Normal", r: 1, c: 0, cSpan: 1, rSpan: 1 },
-			{ name: "XRay" + this.id, command: "true", group: 1, parent: this, action: this.btnXRayClick, text: "X-Ray", r: 1, c: 1, cSpan: 1, rSpan: 1 },
-			{ name: "Isolate" + this.id, command: "true", group: 1, parent: this, action: this.btnIsolateClick, text: "Isolate", r: 1, c: 2, cSpan: 1, rSpan: 1 },
-			{ name: "SelectGroup" + this.id, command: "true", group: 1, parent: this.id, action: this.btnSelectGroupClick, text: "Select Group", r: 1, c: 3, cSpan: 1, rSpan: 1 },
-
-			{ name: "Select" + this.id, command: "true", group: 2, parent: this, action: this.btnSelectClick, text: "Select", r: 2, c: 0, cSpan: 1, rSpan: 1 },
-			{ name: "Dissect" + this.id, command: "true", group: 2, parent: this, action: this.btnDissectClick, text: "Dissect", r: 2, c: 1, cSpan: 1, rSpan: 1 },
-			{ name: "Annotate" + this.id, command: "true", group: 2, parent: this, action: this.btnAnnotateClick, text: "Annotate", r: 2, c: 2, cSpan: 1, rSpan: 1 },
-			{ name: "Reset" + this.id, command: "true", group: 2, parent: this, action: this.btnResetClick, text: "Reset", r: 2, c: 3, cSpan: 1, rSpan: 1 },
-				
-			{ name: "Play" + this.id, command: "true", group: 3, parent: this, action: this.playAnimation, image: path + "play.png", text: "play", r: 3, c: 0, cSpan: 1, rSpan: 1 },
-			{ name: "Pause" + this.id, command: "true", group: 3, parent: this, action: this.pauseAnimation, image: path + "pause.png", text: "pause", r: 3, c: 1, cSpan: 1, rSpan: 1 },
-			{ name: "Previous" + this.id, command: "true", group: 3, parent: this, action: this.previousChapter, image: path + "previous.png", text: "previous", r: 3, c: 2, cSpan: 1, rSpan: 1 },
-			{ name: "Next" + this.id, command: "true", group: 3, parent: this, action: this.nextChapter, image: path + "next.png", text: "next", r: 3, c: 3, cSpan: 1, rSpan: 1 }
-		];
-
-		// getting the height and width of the current container
-		var w = parseInt(this.container.style("width"));
-		var h = parseInt(this.container.style("height"));
-
 		
-		// calculating the heght and width of a single col and row
-		var colW = (w - ((nCols + 2) * padding)) / nCols;
-		var rowH = (h - ((nRows) * padding)) / nRows;
-
-		// setting default stroke and background variables
-		var defaultBg = "#77DD77";
-		var defaultStroke = "white";
-
-		// iterate over the interface model and generates every single item specified before
-		for (var i in this.modelInterface) {
-
-			// getting the styles contained into the model for the current item.
-			// If a style is not specified, it is set to the default value
-			var elem   = this.modelInterface[i];
-			var rSpan  = elem.rSpan || 1;
-			var cSpan  = elem.cSpan || 1;
-			var bg     = elem.backgroundColor || defaultBg;
-			var stroke = elem.stroke || defaultStroke;
-
-			// calculate the actual size with respect to the proportions specified in the model
-			var x = elem.c * (colW + padding) + padding;
-			var y = elem.r * (rowH + padding) + padding;
-			var elemH = rowH * rSpan + (rSpan - 1) * padding;
-			var elemW = colW * cSpan + (cSpan - 1) * padding;
-
-			elem.y = y;
-			elem.h = elemH;
-			elem.x = x;
-			elem.w = elemW;
-
-			// generating the svg item with the specified properties and styles
-			this.container
-				.append("rect")
-				.attr("id", elem.name)
-				.attr("fill", bg)
-				.attr("x", x)
-				.attr("y", y)
-				.attr("width", elemW)
-				.attr("height", elemH)
-				.style("stroke", stroke);
-
-			// adding special attribute
-			if (elem.text) {
-				var t = this.container.append("text")
-					.attr("x", x + elem.w / 2)
-					.attr("y", y + elem.h / 2)
-					.style("dominant-baseline", "middle")
-					.style("text-anchor", "middle")
-					.style("font-size", elem.h * 2 / 3 + "px")
-					.text(elem.text);
-			}
-
-			// inserting the image, if present in the model
-		/*	if (elem.image) {
-				this.container
-					.append("image")
-					.attr("fill", bg)
-					.attr("x", x + 20)
-					.attr("y", y + 5)
-					.attr("width", elem.h - 5)
-					.attr("height", elem.h - 5)
-					.attr("xlink:href", elem.image);
-			}*/
-		}
-	},
-		
-	// adding buttons
 	addWidgetButtons: function() {
 		// adding widget buttons
 		
@@ -211,6 +96,12 @@ var usc_biodigital = SAGE2_App.extend({
 			label: "Pointer",
 			options: ["Sel", "Dis", "Rota"],
 			default: "Rota"
+		});
+
+		this.modelTypeRadioButton = this.controls.addRadioButton({identifier: "ModelType",
+			label: "Model",
+			options: ["Heart", "Male"],
+			default: "Heart"
 		});
 
 		this.controls.finishedAddingControls();
@@ -335,17 +226,6 @@ var usc_biodigital = SAGE2_App.extend({
 		});
 	},
 
-	/*Does nothing different
-	selectGroup: function(){
-		var _this = this;
-		_this.changeTool();
-		_this.human.send("scene.selectionMode", "highlight");
-		    	
-	    _this.human.on("scene.selectionModeUpdated", function(event) {
-			console.log("Enabling " + event.selectionMode + " mode. Click to " + event.selectionMode + " an object");
-		});
-	},*/
-
 	//Replaced by widget events.
 		
 	load: function(date) {
@@ -359,7 +239,7 @@ var usc_biodigital = SAGE2_App.extend({
 	resize: function(date) {
 		// Called when window is resized
 		var w = this.element.clientWidth;
-		var h = this.element.clientHeight;// - 0.1 * this.element.clientHeight - 6;
+		var h = this.element.clientHeight;
 		
 		console.log(this.window + " " + this.isQuiz);
 		if (this.isQuiz) {
@@ -370,13 +250,9 @@ var usc_biodigital = SAGE2_App.extend({
 			this.humanIframe.width = w;
 			//this.btnQuizClick();
 		
-		//this.createBioInterface();
-		//console.log('resize to',  w, h, this.element);
-		
 		this.humanIframe.setAttribute("style", "width:" + w + "px");
 		this.humanIframe.setAttribute("style", "height:" + h + "px");
 		if (this.isQuiz) {
-			console.log(this.humanQuiz);
 			this.humanQuiz.style.fontSize = (this.element.clientWidth / 30 + "px");
 		}
 		this.refresh(date);
@@ -392,7 +268,7 @@ var usc_biodigital = SAGE2_App.extend({
 	},
 
 	// function used to invoke button actions
-	leftClickPosition: function(x, y) {
+	/*leftClickPosition: function(x, y) {
 		// setting the feedback button color
 		var pressedColor = "white";
 		var defaultBg    = "#77DD77";
@@ -447,7 +323,7 @@ var usc_biodigital = SAGE2_App.extend({
 			}
 		}
 
-	},
+	},*/
 	
 	event: function(eventType, position, user_id, data, date) {
 		//console.log('usc_biodigital> eventType, pos, user_id, data, dragging',
@@ -471,7 +347,7 @@ var usc_biodigital = SAGE2_App.extend({
 			this.leftClickPosition(position.x, position.y);
 			var _this = this;
 			//console.log(this.element.clientHeight);
-			var posY = position.y;// - (this.element.clientHeight * 0.1);
+			var posY = position.y;
 			var posX = position.x;
 			// click
 			if (this.tool ==  "default"){
@@ -548,13 +424,12 @@ var usc_biodigital = SAGE2_App.extend({
 			}
 		} else if (eventType === "pointerMove" && this.dragging) {
 			if (this.tool ==  "default"){
-				// move (pan camera)
+				// move (orbit camera)
 				var dx = (position.x - this.dragFromX) * -1;
 				var dy = position.y - this.dragFromY;
 				this.human.send('camera.orbit', { yaw: dx, pitch: dy });
 				this.dragFromX = position.x;
 				this.dragFromY = position.y;
-				// console.log('usc_biodigital> camera.orbit!!', dx, dy);
 				this.refresh(date);
 			}
 		} else if (eventType === "pointerRelease" && (data.button === "left")) {
@@ -601,7 +476,6 @@ var usc_biodigital = SAGE2_App.extend({
 					console.log("usc_biodigital> Reset Widget");
 					this.human.send("scene.reset");
 					this.changeTool();
-					console.log(this.pointerTypeRadioButton);
 					this.pointerTypeRadioButton.value = "Sel";
 					this.viewTypeRadioButton.value = "Norm";
 					break;
@@ -670,6 +544,21 @@ var usc_biodigital = SAGE2_App.extend({
 							console.log('usc_biodigital> Pan Option');
 							this.changeTool();
 							break;*/
+						default:
+							console.log("Error: unknown option");
+							break;
+					}
+					break;
+				case "ModelType":
+					switch (data.value) {
+						case "Heart":
+							console.log("usc_biodigital> Reinitalising with heart model");
+							this.humanIframe.src = "https://human.biodigital.com/widget?be=1to1&background=255,255,255,51,64,77&dk=ae484d517dfdb73aa39b089e0e9f30ff5a1492bb";
+							break;
+						case "Male":
+							console.log("usc_biodigital> Reinitalising with male model");
+							this.humanIframe.src = "https://human.biodigital.com/widget?be=1ud8&background=255,255,255,51,64,77&dk=ae484d517dfdb73aa39b089e0e9f30ff5a1492bb";
+							break;
 						default:
 							console.log("Error: unknown option");
 							break;
